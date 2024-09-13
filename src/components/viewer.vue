b424c75262ef8ca9f46a9f1ca58234527722caf6
leeyoungsoo <ys971119@gmail.com>
2024-09-13 14:05:33 +0900

    cc

---
+++ src/components/viewer.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="viewer">
+    <img
+      src="https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQMsjWNKKuIKkRCKanNnoNJdl73wsCEHsZ0ckzHqqgDy23V5-Fr"
+      alt="Embedded Image"
+    />
+  </div>
+  <div class="ming">원숭희</div>
+</template>
+
+<script>
+export default {
+  name: 'Viewer'
+}
+</script>
+
+<style scoped>
+#viewer {
+  width: 100%;
+  max-width: 600px;
+  margin: 0 auto;
+}
+img {
+  width: 100%;
+  height: auto;
+}
+div.ming {
+  color: pink;
+  font-size: 30px;
+}
+</style>
