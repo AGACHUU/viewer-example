8e1ea5916006b5f6c6cd7fecf95d9e9ef70cdaed
leeyoungsoo <ys971119@gmail.com>
2024-09-20 17:02:56 +0900

    dd

--- src/components/viewer.vue
+++ src/components/viewer.vue
@@ -1,69 +1,52 @@
 <template>
-  <div>
-    <div class="wrap">
-      <img
-        src="https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQMsjWNKKuIKkRCKanNnoNJdl73wsCEHsZ0ckzHqqgDy23V5-Fr"
-        alt="monkey ukkikki"
-        ref="target"
-        class="target"
-      />
-      <div
-        class="target"
-        data-scale="2"
-        data-image="https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQMsjWNKKuIKkRCKanNnoNJdl73wsCEHsZ0ckzHqqgDy23V5-Fr"
-      ></div>
-      <div class="photo" ref="photo"></div>
-    </div>
+  <div class="image-container" @mousemove="handleMouseMove" @mouseleave="handleMouseLeave">
+    <img
+      ref="img"
+      class="magnifiable-image"
+      src="https://mblogthumb-phinf.pstatic.net/MjAyMTEyMjJfNDcg/MDAxNjQwMTUwOTUzODg2.ywMUl6KPyDdljDTufa1npucGWF5Wyl41Q9NhFyM0Xdog.ntOsJiNdGgz7Hd8VQxk1w7onoPz6ztcDPvsie3hsrzog.JPEG.41minit/1640142789090.jpg?type=w800"
+      alt="Magnifiable image"
+    />
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { ref } from 'vue'
 
-<style>
-.wrap {
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-}
-.target {
-  position: relative;
-  width: 500px;
-  height: 500px;
-  margin: 0 auto;
-}
-.photo {
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  background-repeat: no-repeat;
-  background-position: center;
-  background-size: cover;
-  transition: transform 0.5s ease-out;
-}
-</style>
+const img = ref(null)
 
-<script>
-export default {
-  name: 'Viewer'
+const handleMouseMove = (e) => {
+  if (img.value) {
+    //이건 뷰포트에 상대적인 정보를 제고ㅓㅇ하는 객체를 반환함
+    //https://developer.mozilla.org/ko/docs/Web/API/Element/getBoundingClientRect
+    //left, top, right, bottom, x, y, width, height 등값을 픽셀단위로 반환.
+    const rect = img.value.getBoundingClientRect()
+    const x = ((e.clientX - rect.left) / rect.width) * 100
+    const y = ((e.clientY - rect.top) / rect.height) * 100
+
+    img.value.style.transformOrigin = `${x}% ${y}%`
+    img.value.style.transform = 'scale(2)'
+    console.log(e.clientX)
+  }
+}
+
+const handleMouseLeave = () => {
+  if (img.value) {
+    img.value.style.transform = 'scale(1)'
+  }
 }
 </script>
 
 <style scoped>
-#viewer {
-  width: 100%;
-  max-width: 600px;
-  margin: 0 auto;
+.image-container {
+  width: 400px;
+  height: 300px;
+  overflow: hidden;
 }
-img {
+
+.magnifiable-image {
   width: 100%;
-  height: auto;
-}
-div.ming {
-  color: pink;
-  font-size: 30px;
+  height: 100%;
+  object-fit: cover;
+  transition: transform 0.3s ease;
 }
 </style>
